9a9799cdf0f0463e8738260035820246f30365d3
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2025-01-02 00:58:00 +0500

    Anas | Update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import NewCard from '../../components/Card';
+import { RxCross2 } from "react-icons/rx";
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import {
-    Chart as ChartJS,
-    CategoryScale,
+    Chart as ChartJS, CategoryScale,
     LinearScale,
     BarElement,
     Title,
@@ -12,7 +11,6 @@ import {
     Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { Card } from "react-bootstrap";
 
 ChartJS.register(
     CategoryScale, LinearScale,
@@ -95,22 +93,24 @@ const CGPAPredictor = () => {
     return (
         <>
             <Header />
-            <div className="w-full max-w-7xl mx-auto my-5 p-10 bg-gray-100 dark:bg-[#1e1e1e] border-4 border-yellow-400 dark:border-yellow-400 text-[#1e1e1e] dark:text-yellow-400 rounded-lg shadow-2xl shadow-yellow-400">
+            {/* <div className="w-full max-w-7xl mx-auto my-5 p-10 bg-gray-100 dark:bg-[#1e1e1e] border-4 border-yellow-400 dark:border-yellow-400 text-[#1e1e1e] dark:text-yellow-400 rounded-lg shadow-2xl shadow-yellow-400"> */}
+            <div className="container mx-auto my-10">
                 <h1 className="text-center font-bold text-5xl animate-fadeInDown text-yellow-400">CGPA Predictor</h1>
                 <button
-                    className="block w-1/4 mx-auto my-4 p-2 bg-yellow-400 text-[#1e1e1e] dark:bg-yellow-400 dark:text-[#1e1e1e] font-bold uppercase rounded-md hover:bg-gray-100 dark:hover:bg-[#1e1e1e1e] hover:text-yellow-400 dark:hover:text-yellow-400 hover:shadow-lg transform transition-all duration-300"
+                    className="block w-1/4 mx-auto my-4 p-2 bg-yellow-400 text-[#1e1e1e] dark:bg-yellow-400 dark:text-[#1e1e1e] font-bold uppercase rounded-md hover:bg-gray-100 dark:hover:bg-[#1e1e1e1e] hover:text-yellow-400 dark:hover:text-yellow-400  dark:hover:bg-[#1e1e1e] hover:shadow-lg transform transition-all duration-300"
                     onClick={handleAddSubject}
                 >
+                    <div className="bg-teal-300"></div>
                     Add Subject
                 </button>
                 {subjects.map((subject) => (
-                    <div key={subject.id} className="flex justify-between items-center mb-5">
+                    <div key={subject.id} className="flex justify-between items-center gap-5 my-5">
                         <select
                             value={subject.creditHours}
                             onChange={(e) =>
                                 handleInputChange(subject.id, "creditHours", e.target.value)
                             }
-                            className="w-1/3 p-2 border-2 border-yellow-400 dark:border-yellow-400 rounded-md bg-gray-100 dark:bg-[#1e1e1e] text-[#1e1e1e] dark:text-yellow-400 focus:outline-none focus:border-white focus:transform focus:scale-105 transition-all"
+                            className="w-1/3 p-2 border-2 border-yellow-400 dark:border-yellow-400 rounded-md bg-gray-100 dark:bg-[#1e1e1e] text-[#1e1e1e] dark:text-yellow-400 focus:outline-none focus:border-white focus:transform focus:scale-105 scale-100 transition-all"
                         >
                             <option value={1}>1 Credit Hour</option>
                             <option value={2}>2 Credit Hours</option>
@@ -135,12 +135,11 @@ const CGPAPredictor = () => {
                             className="w-1/3 p-2 border-2 border-yellow-400 dark:border-yellow-400 rounded-md bg-gray-100 dark:bg-[#1e1e1e] text-[#1e1e1e] dark:text-yellow-400 focus:outline-none focus:border-white focus:transform focus:scale-105 transition-all"
                         />
                         <button
-                            className="bg-red-500 text-white p-2 rounded-md cursor-pointer border-none hover:bg-red-600 dark:hover:bg-red-600 transform transition-all duration-300"
+                            className="bg-red-500 text-white p-2 rounded-full ms-3 cursor-pointer border-none hover:bg-red-600 dark:hover:bg-red-600 transform transition-all duration-300"
                             onClick={() => handleDeleteSubject(subject.id)}
                         >
-                            Delete
-                        </button>
-                    </div>
+                            <RxCross2 size={22} />
+                        </button></div>
                 ))}
                 <button
                     className="block w-1/4 mx-auto my-4 p-2 bg-yellow-400 text-[#1e1e1e] dark:bg-yellow-400 dark:text-[#1e1e1e] font-bold uppercase rounded-md hover:bg-gray-100 dark:hover:bg-[#1e1e1e] hover:text-yellow-400 dark:hover:text-yellow-400 hover:shadow-lg transform transition-all duration-300"
