878f43955bcf3e83ab63bb1f3b976d2461e0d866
HHARC <hharc123@gmail.com>
2025-01-02 18:26:39 -0800

    hamza++

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CGPAPredictor from "./pages/Home";
+import CGPAPredictor from "./pages/home";
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<CGPAPredictor />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CGPAPredictor />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
